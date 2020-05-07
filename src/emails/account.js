26edfd8973beb5035865620bfb1539781f6eba6d
Hemanshu Mahajan <hemanshu.mahajan@gmail.com>
2020-05-07 22:22:54 +0530

    email id changed

--- src/emails/account.js
+++ src/emails/account.js
@@ -6,16 +6,16 @@ sgMail.setApiKey(sendgridAPIKey);
 const sendWelcomeEmail = (email, name) => {
 	sgMail.send({
 		to: email,
-		from: 'hemanshu.mahajan175@gmail.com',
-		subject: 'Thanks for Joining in!',
-		text: `Welcome to the Task Manager App, ${name}.`
+		from: 'noreply@yodo.com',
+		subject: 'Thanks for Joining YODO!',
+		text: `Welcome ${name} to the YODO App.`
 	});
 };
 
 const sendCancelationEmail = (email, name) => {
 	sgMail.send({
 		to: email,
-		from: 'hemanshu.mahajan175@gmail.com',
+		from: 'noreply@yodo.com',
 		subject: 'Sad to see you  going 😟',
 		text: `What went wrong, ${name}. Can you send me 2 lines that what could I have done to retain you?`
 	});
